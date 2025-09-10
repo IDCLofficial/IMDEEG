8fffc5999c7d6451efa04791f3dc7dc6f21f7b15
jephthahdominic <stdominicjephthah63@gmail.com>
2025-09-10 08:18:42 +0100

    worked on layout changes

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -11,9 +11,9 @@ interface HeroProps {
 
 export const Hero = ({title, caption, subtitle}: HeroProps) => {
     return(
-        <div className={`h-screen px-4 md:px-[3rem] flex flex-col justify-center bg-[url('/images/heroImg.jpg')] bg-fill bg-center`}>
-            <div className="absolute inset-0 bg-black/50 z-0"></div>
-            <div className="relative z-10 py-10">
+        <div className={`relative h-screen px-4 md:px-[3rem] flex flex-col justify-center bg-[url('/images/heroImg.jpg')] bg-fill bg-center`}>
+            <div className="absolute inset-0 bg-black/50 z-0 h-full"></div>
+            <div className="relative z-10 py-10 h-full flex flex-col justify-center">
 
                 <motion.div 
                     key={title}
@@ -44,7 +44,7 @@ interface SubsequentHeroProps {
 export const SubsequentHero = ({ children, className }: SubsequentHeroProps) => {
     return (
         <div className="relative h-[60vh] md:h-[80vh] px-2 md:px-[3rem] py-8 md:py-10 flex flex-col justify-center bg-gradient-to-r from-green-900/20 via-black to-black">
-            <div className={`absolute inset-0 bg-cover bg-center z-0 ${className}`}></div>
+            <div className={`absolute inset-0 bg-cover bg-center h-full z-0 ${className}`}></div>
             {children}
         </div>
     );
